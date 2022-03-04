04dd3b740ff69c3f7d8b17ad108407180091e003
Qasim Ali <35230499+qasim-jutt@users.noreply.github.com>
2022-03-04 21:45:36 +0500

    Update index.js

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Logo from '../../assets/images/skt-logo.svg';
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      <div className="container">
-        
+      <div className="footer-top">
+        <div className="container">
+          <div className="bramd-info">
+            <a href="/" className="brand-logo"><img src={Logo} alt="logo" /></a>
+          </div>
+        </div>
       </div>
     </footer>
   )
